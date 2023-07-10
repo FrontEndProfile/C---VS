2cab97e119b582c3234bc47a5cdc342165486051
Fazal Ur Rehman <fazal.rehman@techsembly.com>
2023-07-10 21:39:32 +0500

    init v--01

---
+++ info.cpp
@@ -0,0 +1,18 @@
+//  #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// } 
+
+
+
+
+
+
+// ====================================
+// int: integer values:: like 1,2,100
+// float: single-precision floating-point values like 11.9 , 100.1
+// double: double-precision floating-point values no idea
+// char: single characters of Number like 65 = A & 66 = B etc..
---
+++ info/app.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // two var sum to show next var 
+  // int x = 5;
+  // int y = 15;
+  // int z = x + y;
+  // cout << z;
+
+
+  // multiple variables in one line
+  // int a,b,c;
+  // a = 10;
+  // b = 1;
+  // c = 14;
+  // int sum = a + b + c ;
+  // cout << sum;
+
+
+
+  // Enter Value of cin , next line show your value "number"
+  // int number;
+  // cout << "Write an integer: ";
+  // cin >> number;
+  // cout << "Your enter " << number << " value" << endl;
+
+
+
+
+  return 0;
+} 
